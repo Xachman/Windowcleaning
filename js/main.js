8c9e03a3a79f5da12913ca2a9119d168ac9a7d7e
Zach Ironside <zironside@hotmail.com>
2015-10-14 07:10:12 -0400

    Added new search clients feature

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 var windowDB = require('windowclean-database');
 var troll = require('windowclean-controller');
 db = windowDB.db;
+
+
+function queryClients(settings) {
+
+  var sql = "SELECT "+settings.fields+" FROM clients";
+  if(settings.like != '' && typeof settings.like != 'undefined'){
+    sql+=" WHERE name LIKE '%"+settings.like+"%'";
+  }
+  if(settings.limit !== '0' && settings.limit !== '' && settings.limit !== 0){
+    sql+=' LIMIT '+settings.limit;
+  }
+  if(settings.off !== '0' && settings.off !== '' && settings.off !== 0){
+    sql+=', '+settings.off;
+  }
+
+  console.log(sql);
+  db.query( sql, function(err, rows, fields) {
+    if (!err){
+      console.log(rows);
+      global.queryOut = rows;
+      $(document).trigger('MySQL');
+      return rows;
+    }
+    else{
+     alert('Error while performing Query: '+err);
+    }
+  });
+}
