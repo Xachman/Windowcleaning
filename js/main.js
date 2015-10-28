9c825015025ad87f533993f411f33903ceec80f5
Zach Ironside <zironside@hotmail.com>
2015-10-28 07:07:52 -0400

    updated to use webapi

--- js/main.js
+++ js/main.js
@@ -10,33 +10,28 @@ var nwin = ngui.Window.get();
 nwin.maximize();
 db = windowDB.db;
 var gui = require("nw.gui");
-
-function queryClients(settings) {
-
-  var sql = "SELECT "+settings.fields+" FROM clients";
-  if(settings.like != '' && typeof settings.like != 'undefined'){
-    sql+=" WHERE name LIKE '%"+settings.like+"%'";
-  }
-  if(settings.limit !== '0' && settings.limit !== '' && settings.limit !== 0){
-    sql+=' LIMIT '+settings.limit;
-  }
-  if(settings.off !== '0' && settings.off !== '' && settings.off !== 0){
-    sql+=', '+settings.off;
-  }
-
-  console.log(sql);
-  db.query( sql, function(err, rows, fields) {
-    if (!err){
-      console.log(rows);
-      global.queryOut = rows;
-      $(document).trigger('MySQL');
-      return rows;
-    }
-    else{
-     alert('Error while performing Query: '+err);
+var thedata;
+function checkLogin(f) {
+  $.get('http://localhost/lask-app.shorewindowcleaning.com?d=status', function(data){
+    data = JSON.parse(data);
+    if(data.login === 0) {
+      f();
+    }else{
+      global.login === true;
     }
   });
 }
+function login() {
+  var f = {};
+  f.email = 'zironside@hotmail.com';
+  f.password = 'picpic';
+  $.post('http://localhost/lask-app.shorewindowcleaning.com/login-processor', f,function(data){
+    console.log(data);
+    checkLogin();
+  });
+}
+checkLogin(login);
+
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -44,9 +39,9 @@ function getParameterByName(name) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function insertDb(table, post) {
-  db.query('INSERT INTO '+table+' SET ?', post, function(err, result) {
-    console.log('result:' + result);
-    console.log('err:' + err);
+function queryClients(settings) {
+  $.post('http://localhost/lask-app.shorewindowcleaning.com/?d=search-clients', settings, function(data){
+    global.clientsJSON = JSON.parse(data);
+    $(document).trigger('data');
   });
 }
